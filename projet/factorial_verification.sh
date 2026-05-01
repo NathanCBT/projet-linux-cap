fe190fc360179344c96f062b0aec5588d8d44180
dejusselm <dejusselm@gmail.com>
2026-05-02 00:25:37 +0200

    Ajout du traitement des messages d'erreurs, et de la vérification des résultats de la factorielle dans un fichier factorial_verification.sh .

---
+++ projet/factorial_verification.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#correction de la factorielle
+note=0
+
+correct_factorial() {
+    nb=$1
+    f=1
+    for (( i=2; i<=nb; i++ )); do
+        f=$((f * i))
+    done
+    echo "$f"
+}
+
+#cas particulier
+
+result=$(./factorielle 0 )
+if [ $result -eq 1 ]; then
+    note=$((note+3))
+fi
+
+#cas général
+
+index=1
+is_result_true=1
+while [ $index -le 10 ] && [ $is_result_true -eq 1 ]; do
+    result=$(./factorielle "$index")
+    true_result=$(correct_factorial "$index")
+    if [ $result -ne $true_result ]; then
+        is_result_true = 0
+    fi
+    index=$((index+1))
+done
+
+if [ $is_result_true = 1 ]; then
+    note=$((note+5))
+fi
+
+#points finaux concernant l'algorithme de la factorielle
+
+echo "$note"
--- projet/mark.sh
+++ projet/mark.sh
@@ -1,63 +1,39 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Mettre en paramètre un nombre"
-    echo "Exemple : $0 <nombre>"
-    exit 1
-fi
-
 make
 
 note=0
 
+#compilation
+
 if test -f "factorielle"; then
     note=$((note+2))
-    echo "La compilation fonctionne"
 else
-    echo "La compilation ne fonctionne pas"
     echo "La note est de : $note"
     exit 1
 fi
 
-#correction de la factorielle
-nb=$1
-f=1
+#messages d'erreur
 
-for (( i=2; i<=nb; i++ )); do
-    f=$((f * i))
-done
-
-echo "Résultat attendue : $f"
-
-#résultat du projet
-result=$(./factorielle "$nb")
-
-echo "Résultat du programme : $result"
-
-if [ "$result" -eq "$f" ]; then
-    echo "Le résultat est correcte"
-    note=$((note+5))
-else
-    echo "Le résultat est faux"
+msg=$(./factorielle)
+if  [ "$msg" = "Erreur: Mauvais nombre de parametres" ]; then
+    note=$((note+4))
 fi
 
-#il faut peut etre le faire de manière automatique car si non l'élève n'aura jamais 20
-if [ "$nb" -eq 0 ]; then
-    if [ "$result" -eq 1 ]; then
-        echo "Le programme gère correctement le cas particulier (factorielle de 0)"
-        note=$((note+3))
-    else
-        echo "Le programme ne gère PAS le cas particulier (factorielle de 0)"
-    fi
+msg=$(./factorielle -16)
+if [ "$msg" = "Erreur: nombre negatif" ]; then
+    note=$((note+4))
 fi
 
 if grep -q "int factorielle( int number )" * ; then
-    echo "Le programme contient une méthode avec la signature int factorielle"
     note=$((note+2))
-else 
-    echo "Le programme ne contient pas de méthode avec la signature int factorielle"
 fi
 
+#points factorielle
+
+note=$((note + $(./factorial_verification.sh)))
+
+#note finale
 
 echo "La note finale est de : $note"
 
