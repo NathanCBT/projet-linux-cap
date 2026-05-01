9dd4a025d5e92bd664ab1b8c059e711931a16f8c
Cobat <nathancobat1@gmail.com>
2026-05-01 12:40:36 +0200

    correction factorielle

--- projet/mark.sh
+++ projet/mark.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
-make 
-./factorielle $1
 
-note=o
+if [ -z "$1" ]; then
+    echo "Mettre en paramètre un nombre"
+    echo "Exemple : $0 <nombre>"
+    exit 1
+fi
 
+make
+
+note=0
 
 if test -f "factorielle"; then
     note=$((note+2))
     echo "La compilation fonctionne"
-    echo "La note est de : $note"
 else
-    echo "La compilation fonctionne pas"
+    echo "La compilation ne fonctionne pas"
     echo "La note est de : $note"
+    exit 1
 fi
 
+#correction de la factorielle
+nb=$1
+f=1
+
+for (( i=2; i<=nb; i++ )); do
+    f=$((f * i))
+done
+
+echo "Résultat attendue : $f"
+
+#résultat du projet
+result=$(./factorielle "$nb")
+
+echo "Résultat du programme : $result"
+
+if [ "$result" -eq "$f" ]; then
+    echo "Le résultat est correcte"
+    note=$((note+5))
+else
+    echo "Le résultat est faux"
+fi
+
+echo "La note finale est de : $note"
+
 make clean
-
-
-
