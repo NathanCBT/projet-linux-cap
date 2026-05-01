72f3548820ad9c9bf15144492c7b7bc3d368b725
Cobat <nathancobat1@gmail.com>
2026-05-01 11:23:07 +0200

    compilation-projet

--- projet/mark.sh
+++ projet/mark.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+make 
+./factorielle $1
+
+note=o
+
+
+if test -f "factorielle"; then
+    note=$((note+2))
+    echo "La compilation fonctionne"
+    echo "La note est de : $note"
+else
+    echo "La compilation fonctionne pas"
+    echo "La note est de : $note"
+fi
+
+make clean
+
+
+
